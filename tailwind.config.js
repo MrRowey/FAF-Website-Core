5b7c7168fbcaa1b9ad6df3f42785b201eb3e12ae
MrRowey <row.josh02@gmail.com>
2023-11-14 22:53:19 +0000

    Add Pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,23 @@ module.exports = {
     ],
     theme: {
       theme: {
+        colors: {
+          'Primary' : '#111',
+          'Secondary' : '#ec9d36',
+          'Link-Main' : '#fff',
+          'Link-Hover' : '#ec9d36',
+          'Link-Dropdown' : '#999',
+          'Danger-Main' : 'fe3939',
+          'Danger-Seconday' : '#ffabab',
+          'Warning-Main': '#faa318',
+          'Warning-Seconday': '#ffe6b3',
+          'Success-Main': '#28c028',
+          'Success-Seconday': '#b3ffb3',
+          'Info-Main': '#26c0c0',
+          'Info-Seconday': '#b3ffff',
+          'Splat-1': '#8e0800',
+          'Splat-2': '#ffa300',
+        },
         extend: {},
       },
     },
